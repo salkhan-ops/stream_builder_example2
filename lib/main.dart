9e8c0150a0bcc06bf0a0baae1c6dd844f4769e31
salkhan-ops <65277891+salkhan-ops@users.noreply.github.com>
2022-05-25 07:27:14 +0500

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,173 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilderExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class StreamBuilderExample extends StatefulWidget {
+  const StreamBuilderExample({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StreamBuilderExample> createState() => _StreamBuilderExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StreamBuilderExampleState extends State<StreamBuilderExample> {
+  final _imageStream = StreamController<Image>();
+  int _imageCount = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List imageList = <Image>[
+    Image.asset(
+      'assets/images/a1.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a2.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a3.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a4.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a5.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a6.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a7.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a8.jpg',
+      fit: BoxFit.cover,
+    ),
+    Image.asset(
+      'assets/images/a9.jpg',
+      fit: BoxFit.cover,
+    ),
+  ];
+
+  @override
+  void dispose() {
+    _imageStream.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(
+          child: Text(
+            'Stream Builder using Image',
+            style: TextStyle(wordSpacing: 3, fontSize: 25),
+          ),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            StreamBuilder(
+                stream: _imageStream.stream,
+                initialData: imageList[0],
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Column(
+                      children: [
+                        Visibility(
+                            visible: snapshot.hasData,
+                            child: ImageDisplayContainer(
+                              snapshot: snapshot,
+                            )),
+                      ],
+                    );
+                  } else if (snapshot.connectionState ==
+                          ConnectionState.active ||
+                      snapshot.connectionState == ConnectionState.done) {
+                    return ImageDisplayContainer(
+                      snapshot: snapshot,
+                    );
+                  } else {
+                    return Text('state: ${snapshot.connectionState}');
+                  }
+                }),
+            SizedBox(height: 100),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  _imageCount == imageList.length - 1
+                      ? _imageCount = 0
+                      : _imageCount++;
+                  _imageStream.sink.add(imageList[_imageCount]);
+                },
+                child: Text(
+                  'Next Image'.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ImageDisplayContainer extends StatelessWidget {
+  final AsyncSnapshot snapshot;
+  const ImageDisplayContainer({
+    super.key,
+    required this.snapshot,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 220,
+      width: 220,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: snapshot.data,
+      ),
     );
   }
 }
